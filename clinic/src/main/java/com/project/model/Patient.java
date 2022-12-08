1317e9884d30d866310bbc87ddece8a784aefbdc
Aguss02 <tosi.agustina02@gmail.com>
2022-12-08 19:02:13 -0300

    DTO for Patients

--- clinic/src/main/java/com/project/model/Patient.java
+++ clinic/src/main/java/com/project/model/Patient.java
@@ -24,20 +24,21 @@ public class Patient {
     @SequenceGenerator(name = "patient_sequence", sequenceName = "patient_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "patient_sequence")
     private Long id;
-
     private String surname;
     private String name;
     private String address;
     private Integer dni;
     private Date registrationDate;
-
     @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Appointment> appointments;
 
-    public Patient(Long id, String rodriguez, String lautaro, String fake_city, int dni, Date date, Appointment appointment) {
+    // Constructor Empty
+   public Patient(){
 
-    }
+   }
 
+    // Constructor with every property
     public Patient(Long id, String surname, String name, String address, Integer dni, Date registrationDate, List<Appointment> appointments) {
         this.id = id;
         this.surname = surname;
@@ -48,5 +49,23 @@ public class Patient {
         this.appointments = appointments;
     }
 
+    // Constructor without appointments
+    public Patient(Long id, String surname, String name, String address, Integer dni, Date registrationDate) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.address = address;
+        this.dni = dni;
+        this.registrationDate = registrationDate;
+    }
+
+    // Constructor without appointments and id
+    public Patient(String surname, String name, String address, Integer dni, Date registrationDate) {
+        this.surname = surname;
+        this.name = name;
+        this.address = address;
+        this.dni = dni;
+        this.registrationDate = registrationDate;
+    }
 
 }
